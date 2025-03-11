2ba914be206df3e55d0b6ad6a5baa5cb464b7953
lpta2302 <lpta2302@gmail.com>
2025-03-11 15:14:49 +0700

    finish

--- user_service/src/main/java/bit/ueh/user_service/UserServiceApplication.java
+++ user_service/src/main/java/bit/ueh/user_service/UserServiceApplication.java
@@ -1,7 +1,11 @@
 package bit.ueh.user_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import bit.ueh.user_service.entity.CCCD;
+import bit.ueh.user_service.repository.CCCDRepository;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,4 +14,16 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner initData(CCCDRepository cccdRepository) {
+        return args -> {
+			CCCD cccd = new CCCD(null, "123456789", "Nguyen Van A", "01/01/1990", "Ho Chi Minh");
+			cccdRepository.save(cccd);
+			System.out.println("✅ CCCD added: " + cccd.getCccdNumber());
+			
+			CCCD cccd2 = new CCCD(null, "987654321", "Tran Thi B", "02/02/1995", "Ha Noi");
+			cccdRepository.save(cccd2);
+			System.out.println("✅ CCCD added: " + cccd2.getCccdNumber());
+        };
+    }
 }
